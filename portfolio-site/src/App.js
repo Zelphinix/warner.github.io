9854cd096a12b0c5b90f9ed6d4bca4e25f9bb4fd
Zelphinix <krystalflare@gmail.com>
2025-03-23 17:51:28 +1300

    Update: Added section folders for individual sections
    Added a section folder containing About, Contact, Projects and Resume folders

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 
 //importing components
 import NavBar from './components/NavBar';
-import AboutMe from './components/About/AboutMe';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
-import Resume from './components/Resume';
+import AboutMe from './Sections/About/AboutMe';
+import Contact from './Sections/Contact/Contact';
+import Projects from './Sections/Projects/Projects';
+import Resume from './Sections/Resume/Resume';
 
 function App() {
   return (
