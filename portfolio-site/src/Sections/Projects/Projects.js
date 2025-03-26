9f7ddcf7f1ca81c051547ab75652242a402e17b4
Zelphinix <krystalflare@gmail.com>
2025-03-26 21:44:45 +1300

    Update Projects.js
    - Update project image link

--- portfolio-site/src/Sections/Projects/Projects.js
+++ portfolio-site/src/Sections/Projects/Projects.js
@@ -4,7 +4,7 @@ import "./Projects.css";
 const projects = [
   {
     name: "Ghostlight Caverns",
-    image: "warner.github.io/portfolio-site/public/ghostlightCaverns.png",
+    image: "https://github.com/Zelphinix/warner.github.io/blob/main/portfolio-site/public/ghostlightCaverns.png?raw=true",
     description: "A puzzle-adventure game exploring logic and shadows.",
     tools: ["Unity", "C#", "Piskel"]
   },
